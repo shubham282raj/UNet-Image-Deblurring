9c0cc9804bc2847800526be1ee953d477374a0af
shubham282raj <shubham282.me@gmail.com>
2024-04-03 03:47:00 +0530

    Added processed dataloader

--- utils/dataloader.py
+++ utils/dataloader.py
@@ -1,4 +1,6 @@
 import os
+from torch.utils.data import Dataset, DataLoader
+import numpy as np
 
 def dataloader():
     dataset_path = "dataset/train/train_sharp"
@@ -19,3 +21,37 @@ def dataloader():
         dataset[folder] = images
 
     return dataset
+
+def processed_dataset():
+
+    dataset_blurred = "dataset/train/train_sharp_processed/blurred"
+    dataset_clear = "dataset/train/train_sharp_processed/clear"
+
+    if (not os.path.exists(dataset_blurred)) or (not os.path.exists(dataset_clear)):
+        print("processed images not available")
+        import sys
+        sys.exit(1)
+
+    dataset = []
+
+    for blur_img in os.listdir(dataset_blurred):
+        
+        clear_img = blur_img.split('_')[0] + ".png"
+
+        blur_img = os.path.join(dataset_blurred, blur_img)
+        clear_img = os.path.join(dataset_clear, clear_img)
+
+        dataset.append((blur_img, clear_img))
+
+    dataset = np.array(dataset)
+    return dataset
+
+class BlurDataset(Dataset):
+    def __init__(self, processed_dataset):
+        pass
+
+    def __getitem__(self, index):
+        pass
+
+    def __len__(self):
+        return self.n_samples
